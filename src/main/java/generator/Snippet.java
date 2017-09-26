21812364e2c66be2b3cd66138956160afb6034eb
Fill a vacancy <xiangjunzhong@qq.com>
2017-09-26 16:15:14 +0800

    -a

---
+++ src/main/java/generator/Snippet.java
@@ -0,0 +1,9 @@
+package generator;
+
+import com.stylefeng.guns.core.shiro.ShiroKit;
+
+public class Snippet {
+    public static void main(String[] args) {
+        System.out.println(ShiroKit.md5("admin", "8pgby"));
+    }
+}
